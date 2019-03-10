cf5d5fde685de1391b704fa41e7ba2eccbb66bac
main <mainuddinrusselbuet73@yahoo.com>
2019-03-10 20:18:45 +0600

    for server side communication

---
+++ index.js
@@ -0,0 +1,78 @@
+// imports firebase-functions module
+const functions = require('firebase-functions');
+// imports firebase-admin module
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+var registrationToken = "estDyQL2mZY:APA91bHaRe5RtaALz6z1-UAKAHCR5rmPsG5epgkOuBxcv9daoeCXqqFuofq6Rpq2pHAfTMR_EO-tForRAXZKuQaUoJ5efTeZlXgf_Es12nngxfQBBXlpbCiOvYqbdm5K1tApJ3zqQkoT"
+
+
+
+
+//admin.initializeApp(functions.config().firebase);
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+ exports.firealarm = functions.database.ref('/noti').onWrite((change,context)=> {
+    console.log('Push notification event triggered');
+    /* Grab the current value of what was written to the Realtime Database */
+  
+    //console.log(event.params.pushId);
+    console.log(change.after.val());
+    
+        var valueObject = change.after.val();
+        console.log(valueObject.title);
+    /*    
+   
+        const payload = {
+            notification: {
+                title: 'App Name',
+                body: "New message",
+                sound: "default"
+            },
+            data : {
+                title: valueObject.title,
+                msg: valueObject.msg
+            }
+            
+        };
+    
+        const options = {
+            priority: "high",
+            timeToLive: 60 * 60 * 24 //24 hours
+        };*/
+        var topic = 'all';
+
+// See documentation on defining a message payload.
+        var message = {
+            data: {
+                title: valueObject.title,
+                msg: valueObject.msg
+            },
+            topic: topic
+        };
+    return admin.messaging().send(message)
+    .then((response) => {
+      // Response is a message ID string.
+      console.log('Successfully sent message:', response);
+      console.log(response.results[0].error);
+     console.log(JSON.stringify(response));
+    })
+    .catch((error) => {
+      console.log('Error sending message:', error);
+    });
+    /*return admin.messaging().sendToDevice(registrationToken,payload,options)
+    .then(function(response){
+     console.log("successfully sent the message",response);
+     console.log(response.results[0].error);
+     console.log(JSON.stringify(response));
+     
+    })
+    .catch(function(error){
+        console.log("error to sent the message",error);
+    });*/
+
+  });
+
+
